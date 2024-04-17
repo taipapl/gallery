39b4b672585070cac84b463e4765418b4881cfa9
Michał Myśliński <michal@taipa.pl>
2024-04-17 22:31:30 +0200

    menu away

--- resources/views/livewire/photos.blade.php
+++ resources/views/livewire/photos.blade.php
@@ -50,63 +50,61 @@ new #[Layout('layouts.app')] class extends Component {
 };
 
 ?>
-<div>
-    <div x-data="{ open: false }">
+<div x-data="{ active: true }">
 
-
-        <div
-            class="fixed right-0 top-0 mr-14 h-screen py-8 overflow-y-auto bg-white border-l border-r w-40 dark:bg-gray-900 dark:border-gray-700">
+    <div x-show="active" @click.away="active = false"
+        class="fixed right-0 top-0 mr-14 h-screen py-8 overflow-y-auto bg-white border-l border-r w-40 dark:bg-gray-900 dark:border-gray-700">
 
 
 
-            <h2 class="px-5 text-lg font-medium text-gray-800 dark:text-white">{{ __('Photos') }}</h2>
+        <h2 class="px-5 text-lg font-medium text-gray-800 dark:text-white">{{ __('Photos') }}</h2>
 
-            <div class="mt-8 space-y-4">
+        <div class="mt-8 space-y-4">
 
-                <livewire:file-uploads />
+            <livewire:file-uploads />
 
-                <x-sub-nav-link href="{{ route('photos.archived') }}">
-                    @lang('Archived Photos')
-                </x-sub-nav-link>
+            <x-sub-nav-link href="{{ route('photos.archived') }}">
+                @lang('Archived Photos')
+            </x-sub-nav-link>
 
-            </div>
         </div>
+    </div>
 
 
-        <div class="py-12">
+    <div class="py-12">
 
 
 
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 text-gray-900">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
 
-                        @if (count($photos) == 0)
-                            <div class="text-center text-lg text-black ">@lang('No photos')</div>
-                        @endif
+                    @if (count($photos) == 0)
+                        <div class="text-center text-lg text-black ">@lang('No photos')</div>
+                    @endif
 
-                        <div class="flex gap-2 flex-wrap">
-                            @php($dataLabel = null)
-                            @foreach ($photos ?? [] as $key => $photo)
-                                @if ($dataLabel != $photo->photo_date->format('F Y'))
-                                    @php($dataLabel = $photo->photo_date->format('F Y'))
-                                    <div class="w-full text-center text-lg text-black ">{{ $dataLabel }}</div>
-                                @endif
-                                <x-photo :photo="$photo" :loop="$loop" />
-                            @endforeach
+                    <div class="flex gap-2 flex-wrap">
+                        @php($dataLabel = null)
+                        @foreach ($photos ?? [] as $key => $photo)
+                            @if ($dataLabel != $photo->photo_date->format('F Y'))
+                                @php($dataLabel = $photo->photo_date->format('F Y'))
+                                <div class="w-full text-center text-lg text-black ">{{ $dataLabel }}</div>
+                            @endif
+                            <x-photo :photo="$photo" :loop="$loop" />
+                        @endforeach
 
-                            <div x-intersect="$wire.loadMore()" class="text-center text-lg text-white "></div>
-
-                        </div>
+                        <div x-intersect="$wire.loadMore()" class="text-center text-lg text-white "></div>
 
                     </div>
+
                 </div>
             </div>
-
-
         </div>
 
+
     </div>
+
+
 
 
 </div>
