4c0498dd68a19119155aa3c90f53e0077262fb41
Michał Myśliński <michal@taipa.pl>
2024-06-09 18:38:27 +0200

    back

--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -26,8 +26,9 @@
                 <div class="flex">
                     <!-- Logo -->
                     <div class="shrink-0 flex items-center">
-
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
+                        <a href="/" wire:navigate>
+                            <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
+                        </a>
 
                     </div>
 
